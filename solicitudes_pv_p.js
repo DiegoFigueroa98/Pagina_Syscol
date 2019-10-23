322f186cab8c7daed2e0fa6fb0cda7132cc634cc
DiegoFigueroa98 <16460315@itcolima.edu.mx>
2019-10-22 23:46:49 -0500

    Se quitó el link a un archivo js que estaba causando conflictos en la pagina de solicitudes pendientes

--- solicitudes_pv_p.js
+++ solicitudes_pv_p.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     $('button').click(function(){
         var activeTab = $(this).attr('href');
-        $('.secciones article:first').show();
         $(activeTab).modal('show');
 		return false;
     });
